6c2c3551d85464702061451b38aa2eb13c0080e9
Shashere <shashank8822@gmail.com>
2026-02-07 11:40:49 +0530

    New File Edit

---
+++ PythonTestFile.py
@@ -0,0 +1,11 @@
+def fibonacci_sequence(n):
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+# Example usage
+n = 10  # Change this value for a different length of the sequence
+print(fibonacci_sequence(n))
